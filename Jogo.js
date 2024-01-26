c0ecd62c51272e3c34d67285f9641e65583fe7e9
Lucas Vam Baster <139773824+lvambaster@users.noreply.github.com>
2024-01-25 23:25:29 -0300

    ajuste das vidas
    ajustando clicks com as vidas do jogo

--- Jogo.js
+++ Jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 
 function areaJogoMataMosquito(){
@@ -18,6 +19,16 @@ function posicaoRandomica(){
     // remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
     document.getElementById('mosquito').remove()
+
+    //console.log('elemento selecionado foi' + vidas)
+  if(vidas>3){
+
+    alert('interromper o jogo (Game Over)')
+  } else{
+    document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+    
+    vidas++
+}
 }
 
 var posicaoX =Math.floor(Math.random() * largura) - 90
@@ -36,6 +47,12 @@ mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.position = 'absolute'
 mosquito.id = 'mosquito'
+
+mosquito.onclick = function(){
+    this.remove()
+}
+
+
 document.body.appendChild(mosquito)
 
 
